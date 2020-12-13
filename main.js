10e90847abe57e3f6f2aae381b333999900d6423
Candice-Leigh-King <candyschofie@gmail.com>
2020-12-13 15:57:19 +0700

    d
    d

--- main.js
+++ main.js
@@ -82,25 +82,6 @@ $(document).ready(function() {
 });
 
 
-		//make sure the page is ready befor running the scripts
-		// This is the hamburger menu
-
-
-// 	var allPanels = $('.accordion > dd').hide();
-//
-// 		$('.accordion > dt > a').click(function() {
-//
-// 			allPanels.slideUp();
-//
-// 	    $(this).filter('.open').removeClass('open').addClass('closing')
-// 	    $('.accordion > dt > a').removeClass('open');
-// 	  $(this).not(".closing").addClass('open').parent().next().slideDown();
-// 	    $('.accordion > dt > a').removeClass('closing');
-//
-// 	    return false;
-// 		});
-//
-//
  });
 
 // end accordian
